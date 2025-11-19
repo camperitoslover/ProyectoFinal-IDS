933ba60557cf02d123c22020b27bebe3a35bb780
camperitoslover <lindaurammolina@gmail.com>
2025-11-18 20:47:40 -0600

    sabadinguez v1

--- Minijuegos/Sabadínguez.py
+++ Minijuegos/Sabadínguez.py
@@ -1,13 +1,52 @@
-print("Juego: Adivina el Significado")
-input("\nPresiona ENTER para continuar")
-print("\nAyuda a Sabadínguez a escribir una historia para su clase de expresión textual")
-input("\nPresiona ENTER para continuar")
+
+
+print("\nJuego: Adivina el Significado")
+input("❥ Presiona ENTER para continuar")
+print("\nAyuda a Sabadínguez a escribir un poema para la feria de literatura")
+input("❥ CONTINUAR")
 print("\nSelecciona el significado correcto de los salvadoreñismos")
-input("\nPresiona ENTER para comenzar el juego")
+input("❥ CONTINUAR")
+
+import random
 
 puntaje = 0
 
-salvadoreñismos = ("Canillera","Amarre","Desteñirse","La de hacer versos","Bisagras")
+salvadoreñismos = {"canillera":"piernas",
+                   "amarre":"no m acuerdo",
+                   "desteñirse":"morir",
+                   "la de hacer versos":"cabeza",
+                   "bisagras":"rodillas"}
 
+letras = ["a", "b", "c"]
 
-print("¡Gracias por ayudarme en mi tarea de Expresión Textual!")
+for salvadoreñismo, significado in salvadoreñismos.items():
+
+    palabras = list(salvadoreñismos.values())
+    (palabras.remove(significado))
+    palabras = random.sample(palabras, 2)
+
+    opciones = [significado] + palabras
+    random.shuffle(opciones)
+
+    print(f"\n¿Qué significa la siguiente palabra?: {salvadoreñismo}")
+
+    for letra, opcion in zip(letras, opciones):
+        print(f"{letra}. {opcion}")
+
+    respuesta = input("\nElige tu respuesta: ").lower()
+
+    indice = letras.index(respuesta)
+    eleccion = opciones[indice]
+
+    if eleccion == significado:
+        print("¡Correcto!")
+        puntaje += 1
+    else:
+        print(f"Incorrecto. La respuesta correcta era: {significado}")
+
+print(f"Has acertado {puntaje} de 5 salvadoreñismos")
+
+if puntaje >= 3:
+    print("¡Gracias por ayudarme en mi poema para la feria de literatura!")
+else:
+    print("Bueno... al menos lo intentaste, ¡lo harás mejor en otra ocasión!")
